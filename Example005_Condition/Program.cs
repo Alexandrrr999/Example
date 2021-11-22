a8b1c7c65e9da8755d83e8b576bb91d0dced846c
John Doe <Alexandrrr999@mail.ru>
2021-11-22 21:55:58 +0500

    program 5

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("Ура, здарова МАХА!!!");
+}
+else 
+{
+    Console.Write("Ты кто нахуй?");
+}
